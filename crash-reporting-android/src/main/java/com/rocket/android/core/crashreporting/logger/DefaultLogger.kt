0d846324fc35ea7bc6311253905dcbf486b0af91
Rui Miguel Alonso <ruiskas@gmail.com>
2021-07-02 13:49:16 +0200

    Fix: improved crash reporter for android

--- crash-reporting-android/src/main/java/com/rocket/android/core/crashreporting/logger/DefaultLogger.kt
+++ crash-reporting-android/src/main/java/com/rocket/android/core/crashreporting/logger/DefaultLogger.kt
@@ -1,36 +1,28 @@
 package com.rocket.android.core.crashreporting.logger
 
-import android.util.Log
+import com.rocket.android.core.crashreporting.printer.LogPrinter
 import com.rocket.core.crashreporting.logger.CrashLogger
 import com.rocket.core.crashreporting.logger.LogLevel
 
-class DefaultLogger(private val debuggable: Boolean = false) : CrashLogger {
+class DefaultLogger(private val debuggable: Boolean = false, private val printer: LogPrinter) : CrashLogger {
 
     override fun log(message: String, map: Map<String, String?>, logLevel: LogLevel) {
         if (debuggable) {
-            val tag = "DefaultLogger"
-            val msg = "$message - $map"
-
-            when (logLevel) {
-                LogLevel.DEBUG -> Log.d(tag, msg)
-                LogLevel.INFO -> Log.i(tag, msg)
-                LogLevel.WARN -> Log.w(tag, msg)
-                LogLevel.ERROR -> Log.e(tag, msg)
-            }
+            printer.printMessage(
+                tag = "DefaultLogger",
+                msg = "$message - $map",
+                logLevel
+            )
         }
     }
 
     override fun log(exception: Throwable, map: Map<String, String?>, logLevel: LogLevel) {
         if (debuggable) {
-            val tag = "DefaultLogger"
-            val msg = "${exception.message} - $map"
-
-            when (logLevel) {
-                LogLevel.DEBUG -> Log.d(tag, msg)
-                LogLevel.INFO -> Log.i(tag, msg)
-                LogLevel.WARN -> Log.w(tag, msg)
-                LogLevel.ERROR -> Log.e(tag, msg)
-            }
+            printer.printMessage(
+                tag = "DefaultLogger",
+                msg = "${exception.message} - $map",
+                logLevel
+            )
         }
     }
 }
