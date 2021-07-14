e042511cfb68ff18f3a2a9e9c76ed9d1631ece1a
Rui Miguel Alonso <ruiskas@gmail.com>
2021-07-14 14:01:11 +0200

    Build: upgraded domain dependency

--- crash-reporting-android-file/src/main/java/com/rocket/android/core/crashreporting/file/reporter/FileLogReporter.kt
+++ crash-reporting-android-file/src/main/java/com/rocket/android/core/crashreporting/file/reporter/FileLogReporter.kt
@@ -1,37 +1,46 @@
 package com.rocket.android.core.crashreporting.file.reporter
 
 import android.app.Application
-import com.rocket.android.core.crashreporting.file.printer.FileLogPrinter
+import com.rocket.android.core.crashreporting.file.di.CoreAndroidFileCrashProvider
 import com.rocket.core.crashreporting.logger.LogLevel
-import kotlinx.coroutines.CoroutineDispatcher
+import com.rocket.core.crashreporting.printer.LogPrinter
+import com.rocket.core.domain.di.CoreProvider.CoreProviderProperty
 
 @Suppress("UnusedPrivateMember")
-class FileLogReporter private constructor(
-    private val fileLogPrinter: FileLogPrinter
-) {
-    fun printMessage(tag: String, msg: String, logLevel: LogLevel) {
-        fileLogPrinter.printMessage(tag, msg, logLevel)
-    }
+class FileLogReporter private constructor(application: Application) {
+    private val debuggable: Boolean
+    private val fileLogPrinter: LogPrinter
 
-    companion object {
-        fun initialize(application: Application, dispatcher: CoroutineDispatcher) {
-            val fileReporter = FileLogReporter(FileLogPrinter(application, dispatcher))
+    init {
+        CoreAndroidFileCrashProvider.getInstance(application).run {
+            debuggable = getPropertyOrNull<Boolean>(CoreProviderProperty.PRINT_LOGS) ?: false
+            fileLogPrinter = logPrinter
+        }
 
-            application.mainLooper.thread.setUncaughtExceptionHandler { thread, throwable ->
-                fileReporter.printMessage(
+        application.mainLooper.thread.setUncaughtExceptionHandler { thread, throwable ->
+            if (debuggable) {
+                fileLogPrinter.printMessage(
                     "FileLogReporter setUncaughtExceptionHandler",
                     throwable.stackTraceToString(),
                     logLevel = LogLevel.ERROR
                 )
             }
+        }
 
-            Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
-                fileReporter.printMessage(
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            if (debuggable) {
+                fileLogPrinter.printMessage(
                     "FileLogReporter setDefaultUncaughtExceptionHandler",
                     throwable.localizedMessage ?: "",
                     logLevel = LogLevel.ERROR
                 )
             }
+        }
+    }
+
+    companion object {
+        fun initialize(application: Application): FileLogReporter {
+            return FileLogReporter(application = application)
         }
     }
 }
