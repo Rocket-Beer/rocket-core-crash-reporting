089c65d18605b55028b36458ec94c112daa307bc
RCASTRSI <rcastrsi@emeal.nttdata.com>
2022-11-14 10:53:08 +0100

    [FEATURE] [RocketCoreCrashReporting] Added Sonar library implementations and his configuration script.

---
+++ buildSrc/src/main/kotlin/sonar.plugin.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("org.sonarqube")
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectVersion", "1.0.0")
+        // This must match "versionName" in the build.gradle of module "presentation".
+        val files = fileTree(baseDir = project.projectDir).apply {
+            setIncludes(listOf("**/jacocoTestReport.xml"))
+        }.files
+        property("sonar.coverage.jacoco.xmlReportPaths", files.joinToString(", "))
+        property("sonar.projectKey", "RocketCoreCrashReporting")
+        property("sonar.projectName", "rocket-core-crash-reporting")
+
+        // Exclusions:
+        // - UI classes are nearly impossible to test because they are highly coupled to Android SDK.
+        property(
+            "sonar.exclusions",
+            "**/*.java" +
+            "**/*.javac"
+        )
+
+    }
+}
