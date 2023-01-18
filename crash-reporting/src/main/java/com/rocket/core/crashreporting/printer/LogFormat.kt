617541f5359e66630b3287c8ab20774bd4492626
afoucart <afoucart@emeal.nttdata.com>
2023-01-18 10:32:57 +0100

    Revert "[FEATURE] Custom file log"
    This reverts commit a49ce08d835c016b18a151bc7e35275fa7b6b369.

--- crash-reporting/src/main/java/com/rocket/core/crashreporting/printer/LogFormat.kt
+++ crash-reporting/src/main/java/com/rocket/core/crashreporting/printer/LogFormat.kt
@@ -1,5 +1,5 @@
 object LogFormat {
     const val currentDateTimeFormat: String = "yyyy-MM-dd HH:mm.sss"
-    const val dateTimeFilenameFormat: String = "yyyy-MM-dd'T'HH:mm:sss"
+    const val dateTimeFilenameFormat: String = "yyyy-MM-dd'T'HH:mm.sss"
     const val currentDateFormat: String = "yyyy-MM-dd"
 }
